40940a3e702cb6abd99f1a34198ce7d4607ccf99
Desmond <des.godgiven.com>
2022-07-28 10:47:34 +0200

    Added give cards method

--- khasino.js
+++ khasino.js
@@ -6,6 +6,30 @@ class Card {
     }
 }
 
+//class for combo card
+class ComboCard {
+    constructor(numberToBuild) {
+        this.numberToBuild = numberToBuild;
+        this.cards = [];
+    }
+}
+
+//class for player
+class Player {
+    constructor() {
+        this.playingCards = [];
+        this.takenCards = [];
+        this.cardBuilding = new ComboCard(0);
+    }
+
+    addPlayingCard = (card) => {
+        if (card !== undefined) {
+            this.playingCards.push(card);
+        }
+        
+    }
+}
+
 //function to create normal deck
 const createFullDeck = () => {
     let length = 10;
@@ -21,7 +45,7 @@ const createFullDeck = () => {
 
     //for loop for hearts
     for (i = 0; i < length; i++) {
-        let newCard = new Card(i+1, "hearts");
+        let newCard = new Card(i+1, "heart");
         deck.push(newCard);
     }
 
@@ -33,7 +57,7 @@ const createFullDeck = () => {
 
     //for loop for trees
     for (i = 0; i < length; i++) {
-        let newCard = new Card(i+1, "tree");
+        let newCard = new Card(i+1, "club");
         deck.push(newCard);
     }
 
@@ -53,7 +77,7 @@ const shuffleCards = (cards) => {
 
         for (let i = 0; i < selectedList.length; i++) {
             if(r === selectedList[i]) {
-                let flag = true;
+                flag = true;
                 break;
             }
         }
@@ -68,10 +92,47 @@ const shuffleCards = (cards) => {
     return deck;
 }
 
+
+//function to distribute cards to players
+const giveCards = (shuffledCards, player1, player2) => {
+    if (shuffledCards.length < 1) {
+        console.log("game over")
+    } else {
+        let length = 10
+        let i = 0;
+
+        for (i; i < length; i++) {
+            player2.addPlayingCard(shuffledCards[shuffledCards.length - 1]);
+            shuffledCards.pop();
+            player1.addPlayingCard(shuffledCards[shuffledCards.length - 1]);
+            shuffledCards.pop();
+        }
+    }
+}
+
 //running code
 let list = createFullDeck();
 let shuffled = shuffleCards(list);
 
+let p1 = new Player();
+let p2 = new Player();
 
+
+
+console.log("The entire cards");
 console.log(list);
+
+
+
+giveCards(shuffled, p1, p2);
+giveCards(shuffled, p1, p2);
+console.log("Cards for player 1");
+console.log(p1.playingCards);
+
+console.log("Cards for player 2");
+console.log(p2.playingCards);
+
+console.log("Shuffled cards");
 console.log(shuffled);
+
+giveCards(shuffled, p1, p2);
